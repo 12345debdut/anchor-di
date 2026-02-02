56308ea00a6d1e580b6bf1bc85beb07056e468e8
Debdut Saha <debdut.saha.1@gmail.com>
2026-02-02 15:41:59 +0530

    Ficed all of the spotless and build issue.

--- composeApp/src/iosArm64Main/kotlin/com/debdut/simpletemplate/di/AnchorSetup.ios.kt
+++ composeApp/src/iosArm64Main/kotlin/com/debdut/simpletemplate/di/AnchorSetup.ios.kt
@@ -3,5 +3,10 @@ package com.debdut.simpletemplate.di
 import com.debdut.anchordi.generated.AnchorGenerated_composeapp
 import com.debdut.anchordi.runtime.ComponentBindingContributor
 
-/** iOS (arm64): returns the KSP-generated contributor for this module. */
+/**
+ * iOS arm64: returns the KSP-generated contributor for this module.
+ *
+ * KSP generates AnchorGenerated_composeapp into the iosArm64Main source set,
+ * so this actual lives here (not in iosMain).
+ */
 actual fun getAnchorContributors(): Array<ComponentBindingContributor> = arrayOf(AnchorGenerated_composeapp)
